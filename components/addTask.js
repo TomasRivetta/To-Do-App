d08e5b793c797972a73ea2201f46ae599965b17f
TomasRivetta <totorivetta@gmail.com>
2022-11-23 15:09:53 -0300

    reorganizando el codigo

--- components/addTask.js
+++ components/addTask.js
@@ -2,70 +2,71 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) => {
-    const list = document.querySelector("[data-list]");
-    const task = createTask(evento);
-    list.appendChild(task);
-  
-  }
-
-  //Arrow functions o funciones anonimas
-  const createTask = (evento) => {
     evento.preventDefault();
-    
-    //Creamos este array para que vaya guardando todas las tareas que creamos
-    // const taskList = []
-
-    //para obtener la informacion del local STORAGE
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    console.log(taskList);
+    const list = document.querySelector("[data-list]");
 
     const input = document.querySelector("[data-form-input]");
     const calendar = document.querySelector("[data-form-date]");
-  
+    
     const value = input.value;
     const date = calendar.value;
-  
     const dateFormat = moment(date).format("DD/MM/YYYY");
-    console.log(dateFormat);
-  
-  
-    const task = document.createElement("li");
-  
-    task.classList.add("card");
-  
+
+    //Creamos este array para que vaya guardando todas las tareas que creamos
+    // const taskList = []
+
     input.value = "";
-  
-    const taskContent = document.createElement("div");
+
+    calendar.value = "";
     
-    //console.log(value,dateFormat);
     const taskObj = {
-      value,
-      dateFormat
+        value,
+        dateFormat
     }
-  
-    taskList.push(taskObj)
-  
-    localStorage.setItem("tasks",JSON.stringify(taskList))
-  
-    
+
+    //para obtener la informacion del local STORAGE
+    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    taskList.push({value,dateFormat});
+
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+
+}
+
+//Arrow functions o funciones anonimas
+const createTask = ({value,dateFormat}) => {
+
+    const task = document.createElement("li");
+
+    task.classList.add("card");
+
+
+    const taskContent = document.createElement("div");
+
+    //console.log(value,dateFormat);
+
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
-  
+
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-  
+
     //taskContent.appendChild(deleteIcon());
     //task.innerHTML = content;
-  
-  
+
+
     const dateElement = document.createElement("span");
     dateElement.innerHTML = dateFormat;
     console.log(dateElement);
-  
+
     task.appendChild(taskContent);
     task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-  
+
     return task;
-  };
+};
